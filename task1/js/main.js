6ef9d250e53cab4a5a66cae326530465d72896dc
Agata Wisniewska <ag.e.wisniewska@gmail.com>
2016-02-25 23:32:18 +0100

    w3 school javascript tutorial examples - supplement

--- task1/js/main.js
+++ task1/js/main.js
@@ -90,3 +90,82 @@ $(document).ready(function() {
 	// document.getElementById('type').innerHTML = typeof 'john';
 // };
 });
+var fruits = ["Banana", "Orange", "Apple", "Mango", "Banana", "Orange", "Apple", "Mango", "Banana", "Orange", "Apple", "Mango", "Banana", "Orange", "Apple", "Mango"];
+
+
+function addingElements() {
+	
+	for (var i=0; i<fruits.length; i++) {
+			var x = ('<td>' + fruits[i] + '<td>');
+		console.log(x);
+	$('#tbody').append('<tr>' + x + '</tr>');
+	}
+}
+
+// trying to add array's methods
+
+var fruits = ["Banana", "Orange", "Purple"];
+
+console.log(fruits.toString());
+
+console.log(fruits.join());
+console.log(fruits.join("+"));
+
+var x = fruits.pop();
+console.log(x);
+console.log(fruits);
+
+var y = fruits.shift()
+console.log(y)
+console.log(fruits);
+var z = fruits.unshift("melon");
+console.log(z);
+console.log(fruits);
+fruits[0] = "strawberry";
+console.log(fruits);
+fruits.splice(1,0, "melon");
+console.log(fruits.toString());
+fruits.splice(2,1);
+console.log(fruits.toString());
+console.log(fruits.sort().toString())
+console.log(fruits.reverse().toString())
+
+// how to sort numbers ascending in javascript 
+// in the same way we can find the lowest value = numbers[0]
+var numbers = [6,7,4,2,9,11];
+var ascending = numbers.sort(function(a,b) { return a-b });
+
+console.log(ascending);
+//  how to sort numbers descending in javascript, in the same way we can find the highest value = numbers[0]
+var numberstwo = [18,12,30,90,27,189];
+var descending = numberstwo.sort(function(a,b) { return b-a });
+console.log(descending);
+
+var commonarray = numberstwo.concat(numbers);
+console.log(commonarray.toString());
+
+var newarray = commonarray.slice(5);
+console.log(newarray.toString())
+// from part of commonarray I create a newestaaray
+
+var newestarray = commonarray.slice(2,4); 
+console.log(newestarray.toString());
+
+ function votable() {
+    
+    var age = document.getElementById("age").value;
+   
+    
+		if (isNaN(age) === false) {
+				var voteable = (age < 18) ? "Too young" : "Old enough";
+		}
+		else if (isNaN(age)) {
+			var voteable = "input error";
+		}
+	// two different text displaying on condition the user's input.
+    // var voteable = (age < 18) ? "Too young":"Old enough";
+    document.getElementById("vote").innerHTML = voteable;
+}
+
+	
+
