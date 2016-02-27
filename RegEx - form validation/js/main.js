642ff26d0ae551faa364abaa35f23376b75cd9f9
Agata Wisniewska <ag.e.wisniewska@gmail.com>
2016-02-27 17:49:57 +0100

    adding a complete form regex validation

--- RegEx - form validation/js/main.js
+++ RegEx - form validation/js/main.js
@@ -1,5 +1,6 @@
 function validateName () {
 	var name = document.getElementById('commentName').value;
+	
 
 	if (name.length == 0) {
 		producePrompt("Name is required", "commentNamePrompt", "commentName", "red", "solid 1px red");
@@ -10,13 +11,113 @@ function validateName () {
 		return false;	
 	}
 
-	producePrompt("Welcome " + name, "commentNamePrompt", "commentName", "green", "solid 1px green");
+	producePrompt("OK!", "commentNamePrompt", "commentName", "green", "solid 1px green");
 	return true;
 
 }
 
-function producePrompt(message, promptLocation,borderLocation, color, border) {
+
+
+function validateSurname () {
+	
+	var surname = document.getElementById('commentLastName').value;
+
+	if (surname.length == 0) {
+		producePrompt("Surname is required", "commentLastNamePrompt", "commentLastName", "red", "solid 1px red");
+		
+		return false;
+	}
+	if (!surname.match(/^[A-z]+$/)) {
+		producePrompt("You can use only characters!", "commentLastNamePrompt", "commentLastName", "red", "solid 1px red" );
+		return false;	
+	}
+
+	producePrompt("OK!", "commentLastNamePrompt", "commentLastName", "green", "solid 1px green");
+	return true;
+
+}
+
+function validatePhone() {
+	var phone = document.getElementById('commentPhone').value;
+
+	if (phone.length == 0) {
+		producePrompt("Phone is required", "commentPhonePrompt", "commentPhone", "red", "solid 1px red");
+		return false;
+	}
+	if (phone.length != 9) {
+		producePrompt("Phone requires 9 digits", "commentPhonePrompt", "commentPhone", "red", "solid 1px red" );
+		return false;	
+	}
+	if (!phone.match(/^[0-9]{9}$/)) {
+		producePrompt("Phone requires digits", "commentPhonePrompt", "commentPhone", "red", "solid 1px red" );
+		return false;	
+	}
+
+	producePrompt("OK!", "commentPhonePrompt", "commentPhone", "green", "solid 1px green");
+	return true;
+
+
+}
+
+
+function validateEmail () {
+	var email = document.getElementById('commentEmail').value;
+
+	if (email.length == 0) {
+		producePrompt("Email is required", "commentEmailPrompt", "commentEmail", "red", "solid 1px red");
+		return false;
+	}
+	if (!email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) {
+		producePrompt("You have to type a proper email address. Check if @ is available", "commentEmailPrompt", "commentEmail", "red", "solid 1px red" );
+		return false;	
+	}
+
+	producePrompt("OK!", "commentEmailPrompt", "commentEmail", "green", "solid 1px green");
+	return true;
+
+
+}
+
+
+
+
+function validateComment() {
+	var comment = document.getElementById('comment').value;
+	var required = 20;
+	var left = required - comment.length;
+
+	if (left > 0) {
+		producePrompt(left + " Characters required", "commentPrompt", "comment", "red", "solid 1px red");
+		return false;
+	}
+
+
+	producePrompt("OK!", "commentPrompt", "comment", "green", "solid 1px green");
+	return true;
+
+}
+
+
+
+function validateForm() {
+
+	if (!validateName() && !validateComment() && !validateEmail () && !validatePhone() && !validateSurname ()) {
+	
+		producePrompt("Form has to be filled correctly", "submitPrompt", "red");
+	
+		return false;
+	}
+}
+
+
+function hidePrompt() {
+		document.getElementById('submitPrompt').style.display = "none";
+}
+
+
+function producePrompt(message, promptLocation, borderLocation, color, border) {
 	document.getElementById(promptLocation).innerHTML = message;
 	document.getElementById(promptLocation).style.color = color;
 	document.getElementById(borderLocation).style.border = border;
 }
+
