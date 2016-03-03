5bdf02ca1f46f02e0440744559c83187c9b0f99b
Agata Wisniewska <ag.e.wisniewska@gmail.com>
2016-03-04 00:02:22 +0100

    UI further working

--- DailyTask project - Learn IT Girl/js/main.js
+++ DailyTask project - Learn IT Girl/js/main.js
@@ -4,6 +4,14 @@ $(document).ready(function() {
 	$("#first").click(function() {
 			$('#form').toggle();
 		});
-	
+	$('#second').click(function() {
+		$('.inner').toggle();
+	})
+		$('#third').click(function() {
+		$('.innertwo').toggle();
+	})
 
 });
+
+
+
